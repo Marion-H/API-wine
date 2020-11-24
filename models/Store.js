ff7edc955f81ab122caffe8d68a37fd63b8ebe65
Marion-H <doudou6500@gmail.com>
2020-11-24 14:25:01 +0100

    add a model Store

---
+++ models/Store.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize")
+const sequelize = require("../sequelize")
+const SequelizeInstance = require("../sequelize")
+
+const Store = SequelizeInstance.define(
+    "Store",
+    {
+        uuid: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: Sequelize.UUIDV4
+        },
+        name: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        }
+    }
+)
+
+module.exports = Store
