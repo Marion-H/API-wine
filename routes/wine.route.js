d44bd20fd9146bda2483fa6a826ea8cd5e1308ff
Marion-H <doudou6500@gmail.com>
2020-11-27 11:21:28 +0100

    update request post wine with many uuid

--- routes/wine.route.js
+++ routes/wine.route.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const wine = express.Router()
 
+const asyncHandler = require('express-async-handler')
+
 const regExpIntegrityCheck = require("../middlewares/regexCheck");
 const { uuidv4RegExp } = require("../middlewares/regexCheck");
 
@@ -11,12 +13,12 @@ const WineStore = require("../models/WineStore")
 
 wine.get("/", async (req, res) => {
     try {
-        const wines = await Wine.findAll({ 
+        const wines = await Wine.findAll({
             include: [{
                 model: Store,
                 as: "stores",
                 required: false,
-                attributes: ["uuid","name"],
+                attributes: ["uuid", "name"],
                 through: {
                     model: WineStore,
                     as: "wineStores",
@@ -42,7 +44,7 @@ wine.get("/:uuid", regExpIntegrityCheck(uuidv4RegExp), async (req, res) => {
                 model: Store,
                 as: "stores",
                 required: false,
-                attributes: ["uuid","name"],
+                attributes: ["uuid", "name"],
                 through: {
                     model: WineStore,
                     as: "wineStores",
@@ -64,31 +66,33 @@ wine.get("/:uuid", regExpIntegrityCheck(uuidv4RegExp), async (req, res) => {
             message: err.message
         })
     }
-}) 
+})
 
-wine.post("/", async (req, res) => {
+wine.post("/", asyncHandler(async (req, res) => {
     const { title, type, image, temperature, region, description, list_dishes, logo, price_indicator, StoreUuid } = req.body
     try {
         const saveWine = await Wine.create({
             title, type, image, temperature, region, description, list_dishes, logo, price_indicator
         })
-        
-        const store = await Store.findByPk(StoreUuid[0])
-        console.log(store.dataValues)
-        const infoWineStore = {
-            WineUuid: saveWine.uuid,
-            StoreUuid: store.uuid 
-        }
-        const saveWineStore = await WineStore.create(infoWineStore)
-        res.status(201).json(saveWine)
-    
-} catch (err) {
-    res.status(422).json({
-        status: "error",
-        message: err
-    })
-}
-})
+
+        const test = await StoreUuid.forEach(async (uuid) => {
+
+            const store = await Store.findByPk(uuid)
+            const infoWineStore = {
+                WineUuid: saveWine.uuid,
+                StoreUuid: store.uuid
+            }
+            const saveWineStore = await WineStore.create(infoWineStore)
+        })
+        return res.status(201).json(saveWine)
+
+    } catch (err) {
+        res.status(422).json({
+            status: "error",
+            message: err
+        })
+    }
+}))
 
 wine.put("/:uuid", async (req, res) => {
     const uuid = req.params.uuid
