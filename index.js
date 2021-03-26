6f6e40e777b7501d3934a41ab5b643a4390f96fc
Marion-H <doudou6500@gmail.com>
2021-03-26 15:30:55 +0100

    add pg for heroku

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const sequelize = require("./sequelize")
-require("./associations/associations")
+const sequelize = require("./sequelize");
+require("./associations/associations");
 
-const User = require("./models/User")
+const User = require("./models/User");
 
-const wine = require("./routes/wine.route")
-const store = require("./routes/store.route")
-const wineStore = require("./routes/wineStore.route")
-const user = require("./routes/user.route")
-const login = require("./routes/login.route")
+const wine = require("./routes/wine.route");
+const store = require("./routes/store.route");
+const wineStore = require("./routes/wineStore.route");
+const user = require("./routes/user.route");
+const login = require("./routes/login.route");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.use(cors())
+app.use(cors());
 
-app.use(helmet())
+app.use(helmet());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/wines", wine)
-app.use("/stores", store)
-app.use("/wineStores", wineStore)
-app.use("/users", user)
-app.use("/login", login)
+app.use("/wines", wine);
+app.use("/stores", store);
+app.use("/wineStores", wineStore);
+app.use("/users", user);
+app.use("/login", login);
 
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome in your API")
-})
+  res.status(200).send("Welcome in your API");
+});
 
-async function main() {
+if (process.env.DATABASE_URL) {
+  const { Client } = require("pg");
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  client.connect();
+} else {
+  async function main() {
     try {
-        await sequelize.sync()
-        await sequelize.authenticate()
-        await User.findCreateFind({
-            where: { user: "Biocoop", password: "toto" },
-        });
-        console.log("Database succesfully joined")
-        app.listen(PORT, (err) => {
-            if (err) throw new Error(err.message)
-            console.log(`Server is running on http://localhost:${PORT}`)
-        })
+      await sequelize.sync();
+      await sequelize.authenticate();
+      await User.findCreateFind({
+        where: { user: "Biocoop", password: "toto" },
+      });
+      console.log("Database succesfully joined");
+      app.listen(PORT, (err) => {
+        if (err) throw new Error(err.message);
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
     } catch (err) {
-        console.log("Unable to join database", err.message)
+      console.log("Unable to join database", err.message);
     }
+  }
 }
 
 if (process.env.NODE_ENV !== "test") {
-    main();
+  main();
 }
 
-module.exports = app
+module.exports = app;
